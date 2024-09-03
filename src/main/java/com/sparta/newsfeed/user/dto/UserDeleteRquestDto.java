2d15b568f10040938e10b73abe166e3d79926114
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-09-03 09:38:04 +0900

    style: 코드 정렬

--- src/main/java/com/sparta/newsfeed/user/dto/UserDeleteRquestDto.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.newsfeed.user.dto;
-
-import lombok.Getter;
-
-@Getter
-public class UserDeleteRquestDto
-{
-  private String password;
-}
--- src/main/java/com/sparta/newsfeed/user/dto/request/UserLoginRequestDto.java
+++ src/main/java/com/sparta/newsfeed/user/dto/request/UserLoginRequestDto.java
@@ -1,10 +1,10 @@
-package com.sparta.newsfeed.user.dto;
+package com.sparta.newsfeed.user.dto.request;
 
 import lombok.Getter;
 
 @Getter
-public class UserLoginRequestDto
-{
+public class UserLoginRequestDto {
+
   private String email;
   private String password;
 }
