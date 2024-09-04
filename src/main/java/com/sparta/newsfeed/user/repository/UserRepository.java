aa56562529fe0e7247a38e454726f2fa953c639f
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-09-04 10:37:16 +0900

    refactor : 유저 삭제 시 비밀번호 수정 및 검증로직 추가

--- src/main/java/com/sparta/newsfeed/user/repository/UserRepository.java
+++ src/main/java/com/sparta/newsfeed/user/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    User findByEmail(String subject);
+    Optional<User> findByEmail(String subject);
 }
